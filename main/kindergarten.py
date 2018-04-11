e51e44eacb0bb8835cc5831800d25d7f25a50378
Bartosz Maciej <bartoszmaciej@Bartoszs-MacBook-Pro.local>
2018-04-11 06:20:05 +0200

    going further, first attempt to search function

--- main/kindergarten.py
+++ main/kindergarten.py
@@ -1,8 +1,24 @@
 import argh
+import os
 
 
-def main():
-    return "I am alive!!!"
+def find(path, name):
+    "Finds folder of file if exists in given path"
+    return findFirst(os.path.expanduser("~"), name)
 
 
-argh.dispatch_command(main)
+def findFirst(path, name):
+    for root, dirs, files in os.walk(path):
+        print(dirs)
+        if name in files:
+            return os.path.join(root, name)
+    else:
+        return "File not found"
+
+
+parser = argh.ArghParser()
+parser.add_commands([findFirst])
+
+
+if __name__ == '__main__':
+    parser.dispatch()
