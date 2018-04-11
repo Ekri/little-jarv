5df9c5b8cc916a65379edc3812b95b05325a1f82
Bartosz Maciej <bartoszmaciej@Bartoszs-MacBook-Pro.local>
2018-04-11 06:35:06 +0200

    playing with shebanks

--- main/kindergarten.py
+++ main/kindergarten.py
@@ -1,24 +1,35 @@
+#!/usr/bin/venv python
+
 import argh
 import os
 
 
-def find(path, name):
-    "Finds folder of file if exists in given path"
-    return findFirst(os.path.expanduser("~"), name)
+def greet(name, greeting='Hello'):
+    "Greets the user with given name. The greeting is customizable."
+    return greeting + ', ' + name
 
-
-def findFirst(path, name):
-    for root, dirs, files in os.walk(path):
-        print(dirs)
-        if name in files:
-            return os.path.join(root, name)
-    else:
-        return "File not found"
+# def list(path):
+#     print(os.listdir(os.getcwd()))
+#
+#
+# def find(path, name):
+#     "Finds folder of file if exists in given path"
+#     return findFirst(os.path.expanduser("~"), name)
+#
+#
+# def findFirst(path, name):
+#     "Finds folder of file if exists in given path"
+#     for root, dirs, files in os.walk(path):
+#         print(dirs)
+#         if name in files:
+#             return os.path.join(root, name)
+#     else:
+#         return "File not found"
 
 
 parser = argh.ArghParser()
-parser.add_commands([findFirst])
-
+# parser.add_commands([findFirst, list])
+parser.add_commands([greet])
 
 if __name__ == '__main__':
     parser.dispatch()
